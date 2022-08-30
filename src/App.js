9345ed49b9e58194ab58b4c9a934c97d5577f2eb
balazsk <balazs.kosa@mediastream.ag>
2022-08-30 23:17:38 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [scores, setScores] = useState([...Array(14)].map(() => 0));
+
+  function getTotalScore() {
+    return scores.reduce((acc, curr, currentIndex) => {
+      return acc + (curr * currentIndex);
+    }, 0);
+  }
+
+  function getTotalShots() {
+    return scores.reduce((acc, curr, currentIndex) => {
+      return acc + curr;
+    }, 0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Grid">
+        {[...Array(14)].map((_, index) => {
+          return (
+            <div className="Item" key={index}>
+              <div className="Counter">{scores[index]}</div>
+              <button onClick={() => {
+                if (getTotalShots() === 15) {
+                  return;
+                }
+
+                setScores((prevState) => {
+                  return prevState.map((item, prevStateIndex) => {
+                    if (prevStateIndex === index) {
+                      return item + 1;
+                    }
+
+                    return item;
+                  })
+                });
+              }}>{index}</button>
+            </div>
+          );
+        })}
+      </div>
+      <div className="Data">
+        <div className="Total">
+          Lövések:<br/>
+          <span>{getTotalShots()}</span>
+        </div>
+        <div className="Total">
+          Pontszám:<br/>
+          <span>{getTotalScore()}</span>
+        </div>
+      </div>
+      <div className="Reset">
+        <button onClick={() => {
+          setScores([...Array(14)].map(() => 0));
+        }}>Nulláz</button>
+      </div>
     </div>
   );
 }
